ab056b1f3b76649a55250d5c37a9ac153132f5d0
Harry <104682768+Harry300408@users.noreply.github.com>
2025-12-01 21:30:32 +0000

    .

--- NOQA/ui/ui_tests.py
+++ NOQA/ui/ui_tests.py
@@ -12,10 +12,11 @@ if __name__ == "__main__":
     test_button = Button((300, 300), "gfx/ui/menus/button/button_bg.png", "gfx/ui/menus/button/button_pressed.png", "Jimmy", 40, (0, 0, 25), (0, 0, 25), "This is a test button being hovered")
     test_button2 = Button((300, 200), "gfx/ui/menus/button/menu_button.png", "gfx/ui/menus/button/menu_button_hovered.png", "Test Button", 40, (255, 100, 25), (0, 0, 25), "This is a test button being hovered")
     test_button3 = Button((300, 400), None, None, "No Img Button", 40, (25, 0, 25), (0, 0, 25), "This is a test button being hovered")
-    switch = switcher((300, 100), None, None, "This is a test switch being hovered")
+    switch = switcher((300, 100), "gfx/ui/menus/switcher/switcher_off.png", "gfx/ui/menus/switcher/switcher_on.png", True, "This is a test switch being \nhovered")
 
     gui_elements = [test_button, test_button2, test_button3, switch, uielement]
     buttons = [test_button, test_button2, test_button3]
+    switches = [switch]
 
     while True:
         screen.fill((0, 0, 0))
@@ -31,6 +32,11 @@ if __name__ == "__main__":
                 for i in buttons:
                     if i.check_for_update():
                         print("Clicked")
+
+                for i in switches:
+                    if i.check_for_update():
+                        i.state = not i.state
+                        print("Switch toggled to", i.state)
 
 
 
