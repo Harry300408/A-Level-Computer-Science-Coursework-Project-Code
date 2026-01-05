df6bc2fb40c662093c14ba422ae6502afd6022a4
Harry <Hazadams2008@hotmail.com>
2026-01-05 23:53:30 +0000

    .

--- NOQA/player/CC.py
+++ NOQA/player/CC.py
@@ -1,6 +1,8 @@
 import pygame, sys, os
 class CC(pygame.sprite.Sprite): # Character Controller
     def __init__(self, groups, x = 0, y = 0):
+        super().__init__(groups)
+        
         self.x = x
         self.y = y
         self.location = [self.x, self.y]
